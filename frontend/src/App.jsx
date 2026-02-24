bfb548bf59cfcfa5cd0c3393f1cea7d4174671c7
monikabaltova <monika.baltova31@gmail.com>
2026-02-24 19:22:10 +0000

    instal and config daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,11 @@
 import { Route, Routes } from "react-router";
-import toast from "react-hot-toast";
 import Home from "./pages/Home";
 import CreateNote from "./pages/CreateNote";
 import NoteDetailPage from "./pages/NoteDetailPage";
 
 function App() {
   return (
-    <div>
-      <button
-        onClick={() => toast.success("weeee")}
-        className="flex items-center justify-between rounded bg-blue-500 p-4 text-white"
-      >
-        Click
-      </button>
+    <div data-theme="cupcake">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateNote />} />
