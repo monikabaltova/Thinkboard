92d5008aba485c84230c1f7f902828e45db65d3a
monikabaltova <monika.baltova31@gmail.com>
2026-03-01 19:41:56 +0000

    fetch notes and create a Loader

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="text-secondary mx-auto mt-6 max-w-7xl p-6 py-6 text-center">
+      Loading...
+    </div>
+  );
+}
+
+export default Loading;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 import Navbar from "../components/Navbar";
+import Loading from "../components/Loading";
 
 function Home() {
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchNotes() {
+      try {
+        const res = await axios.get("http://localhost:8000/api/notes");
+        setNotes(res.data);
+      } catch (error) {
+        console.log("Error fetching notes");
+        toast.error("Failed to load notes");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchNotes();
+  }, []);
+
   return (
     <div className="min-h-screen">
       <Navbar />
+
+      {loading && <Loading />}
     </div>
   );
 }
